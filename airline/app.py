ca9625709ae1ea17cb1ea86f850e26795866d011
JJY <jj2915@nyu.edu>
2021-05-06 22:28:24 +0800

    Update app.py

--- airline/app.py
+++ airline/app.py
@@ -185,7 +185,8 @@ def track_my_spending():
 
     if request.method == "GET":
         TODAY = datetime.today()
-        PAST_YEAR = (TODAY-timedelta(days=365))
+        PAST_YEAR = (TODAY - timedelta(days=365))
+        THIS_YEAR, PAST_YEAR, THIS_MONTH = TODAY.year, TODAY.year - 1, TODAY.month
         month_wise.append([PAST_YEAR.strftime("%Y-%m-%d"), TODAY.strftime("%Y-%m-%d"), 0])
         for i in range(1, 6):
             if THIS_MONTH - i > 0:
